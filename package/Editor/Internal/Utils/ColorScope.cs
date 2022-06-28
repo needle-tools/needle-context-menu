0b8c690a6b74306e534d4a8b7c6a1536ea180dfc
Marcel Wiessler <marcel@gaisterhand.de>
2022-06-28 09:38:11 +0200

    Disabled items gray

---
+++ package/Editor/Internal/Utils/ColorScope.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+
+namespace Needle
+{
+	internal class ColorScope : IDisposable
+	{
+		private readonly Color color;
+
+		public ColorScope(Color col)
+		{
+			this.color = GUI.color;
+			GUI.color = col;
+		}
+		
+		public void Dispose()
+		{
+			GUI.color = this.color;
+		}
+	}
+}
